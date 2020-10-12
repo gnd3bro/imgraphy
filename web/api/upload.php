5ea732f03c2d2cc1464ef10cedb5da0f047c4223
Novang <qkdxorjs1002@naver.com>
2020-10-13 07:33:13 +0900

    썸네일 생성
    works from @rlgus9708
    
    Co-Authored-By: rlgus9708 <71022924+rlgus9708@users.noreply.github.com>

--- web/api/upload.php
+++ web/api/upload.php
@@ -10,7 +10,7 @@
 
     $tmp_dir = "../../tmp/$uuid";
     $img_dir = "../files/img/$uuid";
-    $valid_ext = array('jpg','jpeg','png','gif');
+    $valid_ext = array('jpg','jpeg','png','gif','JPG','JPEG','PNG','GIF');
 
     $exception = $_FILES['uploadfile']['error'];
     $file_name = $_FILES['uploadfile']['name'];
@@ -35,7 +35,52 @@
         echo "{\"code\":\"error\",\"log\":\"tmp file not found\"}";
         exit;
     }
+    
+    $img_name = "$tmp_dir/$uuid.$file_ext";
+    $info = getimagesize($img_name);
+    $mime = $info['mime'];
+    
+    $img_width = (int)trim($info[0]);
+    $img_height = (int)trim($info[1]);
+    
+    $new_img_name = "../files/thumb/$uuid.$file_ext";
+    $new_width = $img_width;
+    $new_height = $img_height;
+    
+    if($img_height > 128){
+        $new_height = 128;
+        $new_width = floor($img_width * $new_height/$img_height);
+    }
 
+    switch ($mime) {
+        case 'imge/jpeg':
+            $create_function = 'imagecreatefromjpeg';
+            $save_function = 'imagejpg';
+            break;
+        case 'image/jpeg':
+            $create_function = 'imagecreatefromjpeg';
+            $save_function = 'imagejpeg';
+            break;
+        
+        case 'image/png':
+            $create_function = 'imagecreatefrompng';
+            $save_function = 'imagepng';
+            break;
+        
+        case 'image/gif':
+            $create_function = 'imagecreatefromgif';
+            $save_function = 'imagegif';
+            break;
+        
+        default:
+            throw new Exception('이미지타입이 없습니다.');
+    }
+    
+    $new_img_resource = imagecreatetruecolor($new_width, $new_height);
+    $img_resource = $create_function($img_name);
+    imagecopyresampled($new_img_resource,$img_resource, 0, 0, 0, 0, $new_width, $new_height, $img_width, $img_height);
+    $save_function($new_img_resource, $new_img_name, 9);
+   
     $db_handle = sql_connect($keypath);
     if(!sql_query_img_insert($db_handle, $uuid, $file_ext, $tag, $license, $uploader)) {
         echo "{\"code\":\"error\",\"log\":\"sql insertion failed\"}";
