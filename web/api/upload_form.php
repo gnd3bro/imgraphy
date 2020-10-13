a3abb11a20e2b8730b325c96fbbb924cdf71242d
Novang <qkdxorjs1002@naver.com>
2020-10-13 17:21:32 +0900

    업로드 폼 양식 변경
    너무 조잡해 보여서 약간 수정
    @rlgus9708

--- web/api/upload_form.php
+++ web/api/upload_form.php
@@ -1,13 +1,19 @@
 <form enctype='multipart/form-data' action='upload.php' method='post'>
-	tag - <input type='text' name='tag'><br />
-	라이센스 -
-        <select name='license'>
-	<option value="1">Attribution (저작자 표시)
-	<option value="2">Noncommercial (비영리)
-	<option value="3">No Derivative Works (변경금지)
-	<option value="4">Share Alike (동일조건변경허락)
-        </select><br />
-	uploader - <input type='text' name='uploader'><br />
+	- tag -<br />
+	<input type='text' name='tag'><br /><br />
+
+	- 라이센스 -<br />
+	<select name='license'>
+		<option value="1">Attribution (저작자 표시)
+		<option value="2">Noncommercial (비영리)
+		<option value="3">No Derivative Works (변경금지)
+		<option value="4">Share Alike (동일조건변경허락)
+	</select><br /><br />
+
+	- uploader -<br />
+	<input type='text' name='uploader'><br /><br />
+
+	- file -<br />
 	<input type='file' name='uploadfile'>
 	<button>upload</button>
 </form>
