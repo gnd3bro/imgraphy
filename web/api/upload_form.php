f4953dafcee2b00b990967b0e8405a719ea3c5a8
Novang <qkdxorjs1002@naver.com>
2020-10-13 17:24:05 +0900

    하이픈 제거

--- web/api/upload_form.php
+++ web/api/upload_form.php
@@ -1,8 +1,8 @@
 <form enctype='multipart/form-data' action='upload.php' method='post'>
-	- tag -<br />
+	tag<br />
 	<input type='text' name='tag'><br /><br />
 
-	- 라이센스 -<br />
+	라이센스<br />
 	<select name='license'>
 		<option value="1">Attribution (저작자 표시)
 		<option value="2">Noncommercial (비영리)
@@ -10,10 +10,10 @@
 		<option value="4">Share Alike (동일조건변경허락)
 	</select><br /><br />
 
-	- uploader -<br />
+	uploader<br />
 	<input type='text' name='uploader'><br /><br />
 
-	- file -<br />
+	file<br />
 	<input type='file' name='uploadfile'>
 	<button>upload</button>
 </form>
