99f48105e8520d412d4abff5f7305c2a5279072e
Novang <qkdxorjs1002@naver.com>
2020-10-13 09:56:40 +0900

    Delete useless file

--- web/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'api.php';
-
-    $keypath = "../../key.json";
-
-    $db_handle = sql_connect($keypath);
-    sql_query_img_insert($db_handle, "test", "testtag", 0, "testuser");
-?>
