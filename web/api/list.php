58ce1f326c7d1f0c0e77127c66ad0d65070f4dac
Novang <qkdxorjs1002@naver.com>
2020-10-22 01:27:11 +0900

    리스트만 반환하도록 수정

--- web/api/list.php
+++ web/api/list.php
@@ -24,8 +24,7 @@
         $array_list = sql_query_img_lookup($db_handle, $keyword, $max, $page);
     }
     
-    $array_count = count($array_list);
     $json_list = json_encode($array_list);
 
-    echo "{\"count\":\"$array_count\",\"list\":$json_list}";
+    echo "$json_list";
 ?>
