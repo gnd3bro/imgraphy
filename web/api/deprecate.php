02e0a1e61a3a35201c80af560efdb3b46cac3313
Novang <qkdxorjs1002@naver.com>
2020-10-22 23:07:12 +0900

    추가된 API 활용

--- web/api/deprecate.php
+++ web/api/deprecate.php
@@ -2,41 +2,21 @@
     require 'api.php';
     header("Content-Type: application/json; charset=UTF-8");
 
-
     $keypath = "../../key.json";
     $confirm = $_GET['confirm'];
     $uuid = $_GET['uuid'];
 
     $handle = sql_connect($keypath);
     
-
-    if($confirm =="true" && $uuid == $uuid) {
-
-        $query = "UPDATE `img_list` SET `deprec`= 1 WHERE `uuid` = '$uuid'";
-        $result = mysqli_query($handle, $query);
-        
-        echo "{\"code\":\"success\",\"log\":\"increase update success\"}";
+    if($confirm != "true" || empty($uuid)) {
+        echo "{\"code\":\"error\",\"log\":\"invalid confirm\"}";
         exit;
-
-    }
-    else if($confirm == "false" && $uuid == $uuid){
-        $query = "UPDATE `img_list` SET `deprec`= 0 WHERE `uuid` = '$uuid'";
-        $result = mysqli_query($handle, $query);
-        echo "{\"code\":\"success\",\"log\":\"decrease update success\"}";
-        exit;
-
-
- 
-
-    }
-    else{
-        echo "{\"code\":\"error\",\"log\":\"confirm empty\"}";
-        exit;
-        
     }
 
+    if(!sql_query_img_set($handle, $uuid, "deprec", true)) {
+        echo "{\"code\":\"error\",\"log\":\"sql update failed\"}";
+        exit;
+    }
 
-
-
-
+    echo "{\"code\":\"success\",\"log\":\"deprec graphy done\"}";
 ?>
