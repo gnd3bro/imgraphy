43c3a0cf60ca9468b3337a75ff6ef1aada2d50f1
rlgus9708 <71022924+rlgus9708@users.noreply.github.com>
2020-11-17 16:13:32 +0900

    코드 오류
    코드 오류해결

--- web/api/convert.php
+++ web/api/convert.php
@@ -4,13 +4,11 @@
 
     header("Content-Type: application/json; charset=UTF-8");
 
-
-    $tmp_dir = "../../tmp/$uuid";
-    $thumb_dir = "../files/thumb/$uuid";
     $img_dir = $argv[1];
     $uuid = $argv[2];
     $file_ext = $argv[3];
-
+    $tmp_dir = "../../tmp/$uuid";
+    $thumb_dir = "../files/thumb/$uuid";
 
     exec("convert $img_dir/$uuid.$file_ext -coalesce -resize '30000@>' $tmp_dir.$file_ext");
 
@@ -18,7 +16,5 @@
         echo "{\"code\":\"error\",\"log\":\"moving file failed\"}";
         exit;
     }
-
-
 
 ?>
